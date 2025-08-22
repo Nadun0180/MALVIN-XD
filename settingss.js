637b8d548223be0d779b38df2b1f56759258c055
Nadun0180 <rashmikanadun48@gmail.com>
2025-08-22 21:52:14 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~JmFQASyR#CHsRwuRSkb6mZdYJhFlvIhysrLv3MmxChDQ5XfgWhE0",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "94776804788", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
